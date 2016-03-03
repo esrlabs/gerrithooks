cd29904eaffeb6328397e0a82f4923ac347a7747
Christian Köstlin <christian.koestlin@esrlabs.com>
2016-03-03 19:07:22 +0100

    improved test.rb framework
    Change-Id: I4a383448942ef1449594eb53b37bf186fede99d9

--- gerrithooks_reviewers_patchsetcreated/test.rb
+++ gerrithooks_reviewers_patchsetcreated/test.rb
@@ -1,6 +1,11 @@
 require 'gerrithooks_reviewers_patchsetcreated'
 def sh(command)
-  puts "running comand '#{command}'"
+  res = `#{command}`
+  exit_status = $?.exitstatus
+  if exit_status != 0
+    raise "'#{command} exited with #{exit_status}"
+  end
+  return res
 end
 args = {'--commit' => '50d4c795e0ce3e2fe99cea38e157b5a2ac76f378', '--project' => '1234'}
 run(args, STDOUT)
