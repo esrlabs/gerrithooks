10484f743a505c3812f596861e9b8ad28d3a5a44
Christian Köstlin <christian.koestlin@esrlabs.com>
2016-03-04 09:25:51 +0100

    streamlined build and install
    Change-Id: I0d87b0524b858301432e0394c7d89c45d5a1f1da

--- rakefile.rb
+++ rakefile.rb
@@ -5,11 +5,21 @@ RSpec::Core::RakeTask.new(:spec)
 
 task :default => :spec
 
-desc 'assemble'
-task :assemble do
+def run_rakefiles(command)
   Dir.glob('**/rakefile.rb').each do |rakefile|
     cd File.dirname(rakefile) do
-      sh "rake clean install"
+      sh "rake #{command}"
     end
   end
 end
+
+desc 'package'
+task :package do
+  run_rakefiles('clean package')
+end
+
+desc 'install_packages'
+task :install_packages do
+  run_rakefiles('install')
+end
+
