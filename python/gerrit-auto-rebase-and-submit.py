64d05fdb481c92ed7d0dea0be8c70f2010283561
Christian Koestlin <christian.koestlin@esrlabs.com>
2018-03-20 14:12:40 +0100

    Adjust to basic auth

--- python/gerrit-auto-rebase-and-submit.py
+++ python/gerrit-auto-rebase-and-submit.py
@@ -1,15 +1,20 @@
-#!/usr/bin/python
-from commands import getoutput
+#!/usr/bin/env python
 import requests
 import json
-from requests.auth import HTTPDigestAuth
+from requests.auth import HTTPBasicAuth
 
-BASE = "http://gerrit/a/"
+BASE = "https://gerrit.int.esrlabs.com/a/"
 PROJECTS = [
-    "your projects",
+    "add",
+    "your",
+    "projects",
+    "here",
 ]
-USER = "..."
-PASSWD = "..."
+USER = ""
+PASSWD = ""
+
+def auth():
+    return HTTPBasicAuth(USER, PASSWD)
 
 def find_rebase_changes(project):
     query = "+".join([
@@ -20,46 +25,47 @@ def find_rebase_changes(project):
         "-label:Code-Review-2",
     ])
     url = "{}changes/?q={}".format(BASE,query)
-    r = requests.get(url, auth=HTTPDigestAuth(USER,PASSWD))
-
-    print url
-    print r.text
+    r = requests.get(url, auth=auth())
+    print(url)
+    print(r.text)
     changes = json.loads(r.text.split("\n",1)[1])
     return changes
 
 def find_submit_change(project):
+    print(project)
     url = "{}changes/?q=project:{}+is:open+is:mergeable".format(BASE,project)
-    r = requests.get(url, auth=HTTPDigestAuth(USER,PASSWD))
+    print(url)
+    r = requests.get(url, auth=auth())
+    if r.status_code != 200:
+        return None
     changes = json.loads(r.text.split("\n",1)[1])
-
+    print(changes)
     for change in changes:
         if change["submittable"]:
             return change
-
     return None
 
 def rebase_change(change):
     url = "{}changes/{}/rebase".format(BASE,change["id"])
-    print url
-    r = requests.post(url, auth=HTTPDigestAuth(USER,PASSWD))
-    print r
+    print(url)
+    r = requests.post(url, auth=auth())
+    print(r)
 
 def submit_change(change):
     url = "{}changes/{}/submit".format(BASE,change["id"])
-    print url
-    r = requests.post(url, auth=HTTPDigestAuth(USER,PASSWD))
-    print r
+    print(url)
+    r = requests.post(url, auth=auth())
+    print(r)
 
 if __name__ == "__main__":
     for project in PROJECTS:
-        print "submitting", project
+        print("submitting", project)
         change = find_submit_change(project)
-        print change
+        print(change)
         if change:
             submit_change(change)
 
-        print "rebasing", project
+        print("rebasing", project)
         for change in find_rebase_changes(project):
-            print change
+            print(change)
             rebase_change(change)
-
